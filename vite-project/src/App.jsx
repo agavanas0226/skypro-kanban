d246c3dd269ede0cb961fb8f73a7467cf918a20f
agavanas0226 <agavanas0226@gmail.com>
2024-01-18 00:08:33 +0600

    помогитеее

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -289,7 +289,7 @@ function App() {
 						<button class="header__btn-main-new _hover01" id="btnMainNew"><a href="#popNewCard">Создать новую задачу</a></button>
 						<a href="#user-set-target" class="header__user _hover02">Ivan Ivanov</a>
 						<div class="header__pop-user-set pop-user-set" id="user-set-target">
-							<!-- <a href="">x</a> -->
+							<!-- <a href="">x(тут ошибку выдает)</a> -->
 							<p class="pop-user-set__name">Ivan Ivanov</p>
 							<p class="pop-user-set__mail">ivan.ivanov@gmail.com</p>
 							<div class="pop-user-set__theme">
