7cc2bc941ec8ccb22aa028a101df0e2b39436838
agavanas0226 <agavanas0226@gmail.com>
2024-01-21 19:21:43 +0600

    Доработка hw2-2

---
+++ vite-project/src/components/Card/Card.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const CardItem = styled.div`
+    padding: 5px;
+    animation-name: card-animation;
+    animation-duration: 500ms;
+    animation-timing-function: linear;
+    `;
