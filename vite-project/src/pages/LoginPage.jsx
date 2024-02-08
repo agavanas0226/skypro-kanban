4bb807361768d69ebfe91f704b64419322295ccc
agavanas0226 <agavanas0226@gmail.com>
2024-02-08 16:48:56 +0600

    немного стилизации добавила

--- vite-project/src/pages/LoginPage.jsx
+++ vite-project/src/pages/LoginPage.jsx
@@ -1,3 +1,29 @@
-export default function LoginPage(){
+import { Link } from "react-router-dom";
+import Wrapper from "../components/Wrapper/Wrapper";
+import { appRoutes } from "../lib/appRoutes";
+import { Container, FormGroup, Modal, ModalBlock, ModalForm, ModalInput, ModalTTL } from "../GlobalStyle";
 
+export default function LoginPage(){
+    return(
+        <Wrapper>
+        <Container>
+            <Modal>
+				<ModalBlock>
+					<ModalTTL>
+						<h2>Вход</h2>
+					</ModalTTL>
+					<ModalForm id="formLogIn" action="#">
+						<ModalInput type="text" name="login" id="formlogin" placeholder="Эл. почта"/>
+						<ModalInput type="password" name="password" id="formpassword" placeholder="Пароль"/>
+						<button className="modal__btn-enter _hover01" id="btnEnter"><a href="../main.html">Войти</a></button>
+						<FormGroup>
+							<p>Нужно зарегистрироваться?</p>
+							<Link to={appRoutes.REGISTER} >Регистрируйтесь здесь</Link>
+						</FormGroup>
+					</ModalForm>
+				</ModalBlock>
+            </Modal>
+        </Container>
+        </Wrapper>
+    )
 }
--- vite-project/src/pages/RegisterPage.jsx
+++ vite-project/src/pages/RegisterPage.jsx
@@ -1,3 +1,29 @@
-export default function RegisterPage(){
+import { Link } from "react-router-dom"
+import Wrapper from "../components/Wrapper/Wrapper"
+import { appRoutes } from "../lib/appRoutes"
+import { Container, FormGroup, Modal, ModalBlock, ModalForm, ModalInput, ModalTTL } from "../GlobalStyle"
 
+export default function RegisterPage(){
+    return(
+        <Wrapper>
+        <Container>
+            <Modal>
+				<ModalBlock>
+					<ModalTTL>
+						<h2>Регистрация</h2>
+					</ModalTTL>
+					<ModalForm>
+						<ModalInput type="text" name="first-name" id="first-name" placeholder="Имя"/>
+						<ModalInput type="text" name="login" id="loginReg" placeholder="Эл. почта"/>
+						<ModalInput type="password" name="password" id="passwordFirst" placeholder="Пароль" />
+						<button className="modal__btn-signup-ent _hover01" id="SignUpEnter"><a href="../main.html">Зарегистрироваться</a> </button>
+						<FormGroup>
+							<p>Уже есть аккаунт?  <Link to={appRoutes.LOGIN}>Войдите здесь</Link></p>
+						</FormGroup>
+					</ModalForm>
+				</ModalBlock>
+			</Modal>
+        </Container>
+        </Wrapper>
+    )
 }
